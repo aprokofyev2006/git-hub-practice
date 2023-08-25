4db60b0ce4781dba2df7549f0dc20bc1d5e0bbe7
aprokofyev2006 <aprokofyev2006@gmail.com>
2023-08-24 20:37:34 -0700

    add sql part 2 procidures and triggers

--- src/main/java/org/example/SqlReviewPart2FunctionsAndProcedures.sql
+++ src/main/java/org/example/SqlReviewPart2FunctionsAndProcedures.sql
@@ -45,3 +45,64 @@ $$
 
 select * from get_department('Clothing');
 
+
+--STORE PROCEDURE
+CREATE OR REPLACE PROCEDURE update_department(emp_id int)
+language plpgsql
+as
+$$
+begin
+update employees set department = 'Toys'
+where employee_id = emp_id;
+commit;
+end;
+$$;
+
+call update_department(10);
+
+select * from employees where employee_id=10;
+
+
+--TRIGGER IS THE EVENT THAT INVOKED AUTOMATICALLY WHEN INSERT, UPDATE, DELETE EVENTS OCCURS
+CREATE TABLE mentors(
+                        id INT GENERATED ALWAYS AS IDENTITY,
+                        first_name varchar(40) not null,
+                        last_name varchar(40) not null ,
+                        primary key(id)
+);
+
+CREATE TABLE mentor_audit(
+                             id INT GENERATED ALWAYS AS IDENTITY,
+                             mentor_id INT not null,
+                             last_name varchar(40) not null ,
+                             changed_on timestamp(6) not null,
+                             primary key(id)
+);
+
+insert into mentors(first_name, last_name) values ('Harold','Finch');
+insert into mentors(first_name, last_name) values ('Severus','Snape');
+
+CREATE OR REPLACE FUNCTION log_last_name_changes()
+    returns trigger
+    language plpgsql
+as
+$$
+begin
+    IF NEW.last_name <> OLD.last_name THEN
+        INSERT INTO mentor_audit(mentor_id,last_name,changed_on) VALUES
+            (OLD.id,OLD.last_name,now());
+end if;
+
+Return NEW;
+end
+$$;
+
+CREATE TRIGGER last_name_change
+    BEFORE update
+    ON mentors
+    FOR EACH ROW
+    EXECUTE PROCEDURE log_last_name_changes();
+
+update mentors
+set last_name ='XYZ'
+where id =2;
